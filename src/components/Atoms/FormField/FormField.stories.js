3e8b1069bb6b2c32a14794bdc8fdf498ca2a7236
pleaobraga <pleao.braga@gmail.com>
2020-11-07 14:51:47 -0300

    Create Select FormField storie

--- src/components/Atoms/FormField/FormField.stories.js
+++ src/components/Atoms/FormField/FormField.stories.js
@@ -2,15 +2,50 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 import { withFormValues } from '../../../../.storybook/withFormValues'
+import MenuItem from '@material-ui/core/MenuItem'
 import { FormField } from './'
+
+const options = ['test1', 'test2']
 
 export default storiesOf('Components | Atom/FormField', module)
   .addDecorator(withFormValues({ test: '' }))
   .add(
-    'default',
+    'Text',
     (props) => (
       <div style={{ background: 'black', width: '300px', padding: '10px' }}>
-        <FormField name="test" label={text('Label', 'Test')} {...props} />
+        <FormField name="test" label={text('Label', 'Text field')} {...props} />
+      </div>
+    ),
+    {
+      info: {
+        inline: true,
+        header: false,
+        source: false,
+        propTables: [FormField],
+        text: `
+          ~~~js
+          <FormField label="Test" name="test" {..props} />
+          ~~~
+      `,
+      },
+    }
+  )
+  .add(
+    'Select',
+    (props) => (
+      <div style={{ background: 'black', width: '300px', padding: '10px' }}>
+        <FormField
+          name="test"
+          label={text('Label', 'Select field')}
+          select
+          {...props}
+        >
+          {options.map((option, index) => (
+            <MenuItem key={`${option}-${index}`} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </FormField>
       </div>
     ),
     {
