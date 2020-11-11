62d1402f4f59fd811a18ed46a202a7e4acb8855b
pleaobraga <pleao.braga@gmail.com>
2020-11-10 21:28:23 -0300

    Update theme primary color

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles'
+import green from '@material-ui/core/colors/green'
 
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: green,
     background: {
       paperHover: '#505050',
     },
