5d8127d1b2d07bf7aeacf8d1cc23239462c7ab95
pleaobraga <pleao.braga@gmail.com>
2020-11-11 13:46:39 -0300

    Update PlaylistCard UI cases

--- src/components/Organism/PlaylistsCard/PlaylistsCard.jsx
+++ src/components/Organism/PlaylistsCard/PlaylistsCard.jsx
@@ -2,14 +2,34 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Card } from '../../Molecule/Card'
-import { selectAllFilteredPlaylists } from '../../../redux/reducer/PlaylistReducer'
+import {
+  selectAllFilteredPlaylists,
+  selectIsFetchingPlaylists,
+} from '../../../redux/reducer/PlaylistReducer'
+import Typography from '@material-ui/core/Typography'
+import Loading from '../../Atom/Loading'
 
 import { StyledPlaylistsCard } from './styles'
 
 const PlaylistsCard = ({ className }) => {
   const playlists = useSelector(selectAllFilteredPlaylists)
+  const isFetching = useSelector(selectIsFetchingPlaylists)
+  const hasError = useSelector(selectIsFetchingPlaylists)
 
-  return (
+  if (isFetching) return <Loading />
+
+  if (hasError) {
+    return (
+      <Typography color="textPrimary">
+        Houve um erro ao apresentar o resultado, confira os filtros e tente
+        novamente
+      </Typography>
+    )
+  }
+
+  return playlists.length === 0 ? (
+    <Typography color="textPrimary">Nenhum item a ser exibido.</Typography>
+  ) : (
     <StyledPlaylistsCard className={className}>
       {playlists.map(({ external_urls, id, name, images }) => {
         return (
