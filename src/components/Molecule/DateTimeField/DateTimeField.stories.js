a4f02467fafcf65b99ab8cfaf257357175f1880c
pleaobraga <pleao.braga@gmail.com>
2020-11-12 14:50:57 -0300

    Update DateTimeField storybook

--- src/components/Molecule/DateTimeField/DateTimeField.stories.js
+++ src/components/Molecule/DateTimeField/DateTimeField.stories.js
@@ -2,12 +2,9 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 import { withFormValues } from '../../../../.storybook/withFormValues'
-import MenuItem from '@material-ui/core/MenuItem'
 import { DateTimeField } from '.'
 
-const options = ['test1', 'test2']
-
-export default storiesOf('Components | Molecule/FormField', module)
+export default storiesOf('Components | Molecule/DateTime', module)
   .addDecorator(withFormValues({ test: '' }))
   .add(
     'Text',
@@ -15,7 +12,7 @@ export default storiesOf('Components | Molecule/FormField', module)
       <div style={{ background: '#303030', width: '300px', padding: '10px' }}>
         <DateTimeField
           name="test"
-          label={text('Label', 'Text field')}
+          label={text('Label', 'Date field')}
           {...props}
         />
       </div>
@@ -29,42 +26,6 @@ export default storiesOf('Components | Molecule/FormField', module)
         text: `
           ~~~js
           <DateTimeField label="Test" name="test" {..props} />
-          ~~~
-      `,
-      },
-    }
-  )
-  .add(
-    'Select',
-    (props) => (
-      <div style={{ background: '#303030', width: '300px', padding: '10px' }}>
-        <DateTimeField
-          name="test"
-          label={text('Label', 'Select field')}
-          select
-          {...props}
-        >
-          {options.map((option, index) => (
-            <MenuItem key={`${option}-${index}`} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </DateTimeField>
-      </div>
-    ),
-    {
-      info: {
-        inline: true,
-        header: false,
-        source: false,
-        propTables: [DateTimeField],
-        text: `
-          ~~~js
-          <DateTimeField label="Test" name="test" select {..props} >
-            <MenuItem value={option}>
-              {name}
-            </MenuItem>
-          </DateTimeField>
           ~~~
       `,
       },
