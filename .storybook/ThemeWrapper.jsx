cb1fa05a8bb39b5c21fca1833f2d52c1ce84b5d4
pleaobraga <pleao.braga@gmail.com>
2020-11-12 14:50:30 -0300

    Update Storybook configuration

--- .storybook/ThemeWrapper.jsx
+++ .storybook/ThemeWrapper.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import { ThemeProvider } from 'styled-components'
 import theme from '../src/theme'
+import DateFnsUtils from '@date-io/date-fns'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 
 const ThemeWrapper = ({ children, store }) => (
   <MuiThemeProvider theme={theme}>
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        {children}
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>
   </MuiThemeProvider>
 )
 
