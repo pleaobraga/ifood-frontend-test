95d654d83868bab899870b1fb1aab05c130ceba4
pleaobraga <pleao.braga@gmail.com>
2020-11-13 14:05:40 -0300

    Update filter url

--- src/api/filters.js
+++ src/api/filters.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const baseURL = 'http://www.mocky.io/v2/5a25fade2e0000213aa90776'
+const baseURL = 'https://www.mocky.io/v2/5a25fade2e0000213aa90776'
 
 export const getFilterAPI = () => axios.get(baseURL)
